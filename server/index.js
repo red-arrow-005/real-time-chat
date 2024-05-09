efd6cb797008c92c35a4ad2f6b1c63449a418f24
red-arrow-005 <karthik.a2219905@gmail.com>
2024-05-09 14:02:53 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle incoming messages
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    // Handle user disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
