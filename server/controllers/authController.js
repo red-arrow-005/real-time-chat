185f88476efaad7895ffd1ac8ac173e5d5c1c6ac
red-arrow-005 <karthik.a2219905@gmail.com>
2024-05-14 14:23:05 +0530

    "Add success field to authController responses and update start script in package.json to use nodemon"
    This commit includes changes to the authController.js file where the response objects for various endpoints have been updated to include a "success

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,17 +8,17 @@ exports.login = async (req, res) => {
     try {
         passport.authenticate('local', async (err, user, info) => {
             if (err) {
-                return res.status(500).json({ message: 'Authentication error' });
+                return res.status(500).json({ success: false, message: 'Authentication error' });
             }
             if (!user) {
-                return res.status(401).json({ message: 'Invalid email or password' });
+                return res.status(401).json({ success: false, message: 'Invalid email or password' });
             }
             req.login(user, { session: false }, async (error) => {
                 if (error) {
-                    return res.status(500).json({ message: 'Login error' });
+                    return res.status(500).json({ success: false, message: 'Login error' });
                 }
                 const token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '1h' });
-                return res.json({ token });
+                return res.json({ success: true, message: 'Login successfully', data: { user, token } });
             });
         })(req, res);
     } catch (error) {
