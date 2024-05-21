6d9f70e9a9dcbafbc3472be0608d50bd593ba9c0
red-arrow-005 <karthik.a2219905@gmail.com>
2024-05-21 15:43:14 +0530

    "Updated App.js to add DashboardPage and conditional routing based on user profile, and added console log in api/index.js"

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AuthPage from './pages/AuthPage'; // Import the authentication page component
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('profile'));
   return (
     <Router>
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="/auth" />} />
-          <Route exact path="/auth" element={<AuthPage />}>
+          <Route path='/dashboard' Component={() => (!user ? <AuthPage /> : <DashboardPage />)} />
+          <Route exact path="/auth" Component={() => (!user ? <AuthPage /> : <Navigate to="/dashboard" />)}>
           </Route>
         </Routes>
       </div>
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,7 +8,7 @@ API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
-
+    console.log("first", req)
     return req;
 });
 
