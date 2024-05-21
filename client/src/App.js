59ff10c423f1fa0348f0d7e1764c522813cabb63
red-arrow-005 <karthik.a2219905@gmail.com>
2024-05-21 14:11:18 +0530

    "Added React Router to App.js, replaced direct AuthPage rendering with routing configuration"

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import AuthPage from './pages/AuthPage';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthPage from './pages/AuthPage'; // Import the authentication page component
 
 function App() {
   return (
-    <div>
-      <AuthPage />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/auth" />} />
+          <Route exact path="/auth" element={<AuthPage />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
