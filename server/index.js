521121b5e3b20584e373d159ca1a4e99713e4ebc
red-arrow-005 <karthik.a2219905@gmail.com>
2024-05-23 13:15:08 +0530

    "Remove Socket.IO and related code from server/index.js"

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
@@ -12,7 +11,6 @@ require('./config/passport')(passport); // Passport config
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,37 +29,6 @@ app.use(cors({
 
 // Routes
 app.use('/v1/api', routes);
-
-// JWT Authentication Middleware for Socket.IO
-io.use((socket, next) => {
-    const token = socket.handshake.query.token;
-    if (token) {
-        jwt.verify(token, 'secret', (err, decoded) => {
-            if (err) {
-                return next(new Error('Authentication error'));
-            }
-            socket.user = decoded;
-            next();
-        });
-    } else {
-        next(new Error('Authentication error'));
-    }
-});
-
-// Socket.IO Connection
-io.on('connection', (socket) => {
-    console.log('A user connected:', socket.user.id);
-
-    // Handle incoming messages
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', { user: socket.user.username, msg });
-    });
-
-    // Handle user disconnection
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.user.id);
-    });
-});
 
 // MongoDB Connection
 mongoose.connect('mongodb://localhost:27017/chat-app').then(() => {
